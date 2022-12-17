82450981e9598cb234301316dbfb4f3a3de813c6
Krypt0niT <k269692@gmail.com>
2022-12-17 12:52:06 +0100

    anim

--- My project/Assets/Player.cs
+++ My project/Assets/Player.cs
@@ -9,6 +9,16 @@ public class Player : MonoBehaviour
     CharacterController chc;
     manager managerVariables;
 
+    SpriteRenderer PlayerRend;
+
+    int playerRotation = 0;
+    string animState = "idle";
+
+
+    [SerializeField] List<Sprite> Beh = new List<Sprite>();
+    float behTick = 0;
+    float behSpeed = 7;
+    int behIndex = 0;
     float moveX = 0;
     void Start()
     {
@@ -16,20 +26,58 @@ public class Player : MonoBehaviour
         controls = managerObject.GetComponent<Controls>();
         managerVariables = managerObject.GetComponent<manager>();
         chc = this.gameObject.GetComponent<CharacterController>();
+        PlayerRend = this.gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(controls.PlayerLeft))
+        {
             moveX = -1;
+            playerRotation = 180;
+            animState = "running";
+        }
+            
         else if (Input.GetKey(controls.PlayerRight))
+        {
             moveX = 1;
+            playerRotation = 0;
+            animState = "running";
+
+        }
+
         else
+        {
             moveX = 0;
+            animState = "idle";
+
+        }
 
         chc.Move(new Vector3(moveX,0,0) * Time.deltaTime * managerVariables.PlayerStats.speed);
+        transform.rotation = Quaternion.Euler(0, playerRotation,0);
 
 
+
+        //animacie
+        if (animState == "running")
+        {
+            behTick += Time.deltaTime;
+            if (behTick >= 1 / behSpeed)
+            {
+                PlayerRend.sprite = Beh[behIndex];
+                behTick = 0;
+                if (behIndex < Beh.Count - 1)
+                {
+                    behIndex++;
+                }
+                else
+                {
+                    behIndex = 0;
+                }
+
+            }
+        }
+        
     }
 }
--- My project/Assets/manager.cs
+++ My project/Assets/manager.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class manager : MonoBehaviour
 {
     // Start is called before the first frame update
+    public bool raid = false;
+    int dayTime = 0;
+    float dayTick = 0;
 
+    int RaidTime = 20;
+    int RaidLength = 0;
+    
 
+    [SerializeField] GameObject timer;
+    [SerializeField] GameObject timerBG;
     public class Player
     {
-        public float speed = 10f;
+        public float speed = 5f;
     }
     public Player PlayerStats = new Player();
     void Start()
@@ -20,6 +29,35 @@ public class manager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        dayTick += Time.deltaTime;
+        if (dayTick >= 1)
+        {
+            
+            
+            if (dayTime == 60)
+            {
+                timerBG.GetComponent<SpriteRenderer>().color = Color.red;
+                raid = true;
+                RaidLength++;
+                dayTick = 0;
+                if (RaidLength >= RaidTime)
+                {
+                    raid = false;
+                    RaidLength = 0;
+                    dayTime = 0;
+                    timer.GetComponent<TextMeshPro>().text = dayTime.ToString();
+                    timerBG.GetComponent<SpriteRenderer>().color = Color.green;
+                }
+            }
+            else
+            {
+                dayTime++;
+                dayTick = 0;
+                timer.GetComponent<TextMeshPro>().text = dayTime.ToString();
+                timerBG.GetComponent<SpriteRenderer>().color = Color.green;
+            }
+        }
+        
 
     }
 }
