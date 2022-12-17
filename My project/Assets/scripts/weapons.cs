110c3bef23787946326ce0b6ea0f6afe341405dd
Krypt0niT <k269692@gmail.com>
2022-12-17 16:28:01 +0100

    TURRETA

--- My project/Assets/scripts/weapons.cs
+++ My project/Assets/scripts/weapons.cs
@@ -7,6 +7,8 @@ public class weapons : MonoBehaviour
 
     Vector2 mousePos;
     [SerializeField] Transform weaponPointer;
+    [SerializeField] GameObject weapon1;
+    [SerializeField] GameObject weapon2;
 
     void Start()
     {
@@ -16,9 +18,27 @@ public class weapons : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        mousePos = Input.mousePosition;
-        print(mousePos);
-        weaponPointer.LookAt(mousePos);
+        if (GameObject.Find("Player").GetComponent<Player>().usingWeapons)
+        {
+            if (Input.mousePosition.x > 960)
+            {
+                weapon1.SetActive(true);
+                weapon2.SetActive(false);
+
+            }
+            if (Input.mousePosition.x < 960)
+            {
+                weapon1.SetActive(false);
+                weapon2.SetActive(true);
+
+            }
+            var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(new Vector3(weaponPointer.position.x, weaponPointer.position.y, weaponPointer.position.z));
+            var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            weaponPointer.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        }
+        
+      
 
     }
 }
