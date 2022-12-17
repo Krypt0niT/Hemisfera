07eba498b4c72873ffb605cac282cc8daeb4030c
NikolasMichalicka <michalickanikolas@gmail.com>
2022-12-18 00:06:47 +0100

    a tak

--- My project/Assets/scripts/Controls.cs
+++ My project/Assets/scripts/Controls.cs
@@ -12,12 +12,25 @@ public class Controls : MonoBehaviour
     public bool PlayerRight;
     public bool PlayerLeft;
     public bool PlayerUse;
+    public bool PlayerUse2;
+    public float PlayerUseTime = 0f;
     public bool PlayerAttack;
     private void Update()
     {
         PlayerAttack = Input.GetKey(PlayerAttackKey);
         PlayerRight = Input.GetKey(PlayerRightKey) && !PlayerAttack;
         PlayerLeft = Input.GetKey(PlayerLeftKey) && !PlayerAttack;
-        PlayerUse = Input.GetKeyDown(PlayerUseKey);
+        PlayerUse2 = Input.GetKey(PlayerUseKey);
+        PlayerUseTime += Time.deltaTime;
+    }
+
+    private void FixedUpdate()
+    {
+        if(PlayerUseTime > 1f && PlayerUse2)
+        {
+            PlayerUse = true;
+            PlayerUseTime = 0f;
+        }
+        else PlayerUse = false;
     }
 }
