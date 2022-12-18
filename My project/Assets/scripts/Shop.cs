63c8fe64f85d612d363ea2cdb94f1815c723694a
NikolasMichalicka <michalickanikolas@gmail.com>
2022-12-18 13:29:58 +0100

    test build

--- My project/Assets/scripts/Shop.cs
+++ My project/Assets/scripts/Shop.cs
@@ -35,11 +35,11 @@ public class Shop : MonoBehaviour
         public string[] hardnessDescription = new string[4] { "Unlocks the ability to mine Iron", "Unlocks the ability to mine Gold", "Unlocks the ability to mine Diamond", "Unlocks the ability to mine Chunkerium" };
         public string[] hardnessCost = new string[4] { "0:10, , ", "0:20,2:6, ", "0:20,2:10,4:5", "2:20,4:15,5:5" };
 
-        //===== BASE =====//
+        //===== BASE =====// ???????????????????????????????????? dole treba robit
 
         //=== shield ===//
         public bool[] shieldBool = new bool[5] { false, false, false, false, false };
-        public string[] shieldName = new string[5] { "Damage I", "Damage II", "Damage III", "Damage IV", "Damage V" };
+        public string[] shieldName = new string[5] { "Shield I", "Shield II", "Shield III", "Shield IV", "Shield V" };
         public string[] shieldDescription = new string[5] { "Drill damage + 10", "Drill damage + 40", "Drill damage + 120", "Drill damage + 300", "Drill damage + 800" };
         public string[] shieldCost = new string[5] { "0:5, , ", "0:10,1:6, ", "2:10,3:5, ", "2:15,4:10,5:6", "4:20,5:10,6:3" };
 
@@ -49,11 +49,45 @@ public class Shop : MonoBehaviour
         public string[] healthDescription = new string[5] { "Player speed + 3", "Player speed + 5", "Player speed + 8", "Player speed + 12", "Player speed + 16" };
         public string[] healthCost = new string[5] { "0:5, , ", "0:10,1:6, ", "2:10,3:5, ", "2:15,4:10,5:6", "4:20,5:10,6:3" };
 
-        //=== repair ===//
-        public int[] RepairCost = new int[] { 2, 1 };
-        public string[] repairName = new string[4] { "Drill I", "Drill II", "Drill III", "Drill IV" };
-        public string[] repairDescription = new string[4] { "Unlocks the ability to mine Iron", "Unlocks the ability to mine Gold", "Unlocks the ability to mine Diamond", "Unlocks the ability to mine Chunkerium" };
-        public string[] repairCost = new string[1] { "0:10, , " };
+        //=== repair ===// toto netreba
+        public static int[] RepairCost = new int[] { 2, 1 };
+        public string[] repairName = new string[1] { "Repair"};
+        public string[] repairDescription = new string[1] { "Repairs base to 100%"};
+        public string[] repairCost = new string[1] { "2:"+RepairCost[0].ToString()+ ", 4:"+RepairCost[0].ToString()+", " };
+
+        //===== Main weapon =====//
+
+        //=== Damage ===//
+        public bool[] MainDamageBool = new bool[5] { false, false, false, false, false };
+        public string[] MainDamageName = new string[5] { "Shield I", "Shield II", "Shield III", "Shield IV", "Shield V" };
+        public string[] MainDamageDescription = new string[5] { "Drill damage + 10", "Drill damage + 40", "Drill damage + 120", "Drill damage + 300", "Drill damage + 800" };
+        public string[] MainDamageCost = new string[5] { "0:5, , ", "0:10,1:6, ", "2:10,3:5, ", "2:15,4:10,5:6", "4:20,5:10,6:3" };
+
+        //=== Speed ===//
+        public bool[] MainSpeedBool = new bool[5] { false, false, false, false, false };
+        public string[] MainSpeedName = new string[5] { "Speed I", "Speed II", "Speed III", "Speed IV", "Speed V" };
+        public string[] MainSpeedDescription = new string[5] { "Player speed + 3", "Player speed + 5", "Player speed + 8", "Player speed + 12", "Player speed + 16" };
+        public string[] MainSpeedCost = new string[5] { "0:5, , ", "0:10,1:6, ", "2:10,3:5, ", "2:15,4:10,5:6", "4:20,5:10,6:3" };
+
+        //=== Accuracy ===//
+        public bool[] MainAccuracyBool = new bool[5] { false, false, false, false, false };
+        public string[] MainAccuracyName = new string[5] { "Speed I", "Speed II", "Speed III", "Speed IV", "Speed V" };
+        public string[] MainAccuracyDescription = new string[5] { "Player speed + 3", "Player speed + 5", "Player speed + 8", "Player speed + 12", "Player speed + 16" };
+        public string[] MainAccuracyCost = new string[5] { "0:5, , ", "0:10,1:6, ", "2:10,3:5, ", "2:15,4:10,5:6", "4:20,5:10,6:3" };
+
+        //===== Secondary weapons =====//
+
+        //=== Damage ===//
+        public bool[] SecondaryDamageBool = new bool[5] { false, false, false, false, false };
+        public string[] SecondaryDamageName = new string[5] { "Shield I", "Shield II", "Shield III", "Shield IV", "Shield V" };
+        public string[] SecondaryDamageDescription = new string[5] { "Drill damage + 10", "Drill damage + 40", "Drill damage + 120", "Drill damage + 300", "Drill damage + 800" };
+        public string[] SecondaryDamageCost = new string[5] { "0:5, , ", "0:10,1:6, ", "2:10,3:5, ", "2:15,4:10,5:6", "4:20,5:10,6:3" };
+
+        //=== Speed ===//
+        public bool[] SecondarySpeedBool = new bool[5] { false, false, false, false, false };
+        public string[] SecondarySpeedName = new string[5] { "Speed I", "Speed II", "Speed III", "Speed IV", "Speed V" };
+        public string[] SecondarySpeedDescription = new string[5] { "Player speed + 3", "Player speed + 5", "Player speed + 8", "Player speed + 12", "Player speed + 16" };
+        public string[] SecondarySpeedCost = new string[5] { "0:5, , ", "0:10,1:6, ", "2:10,3:5, ", "2:15,4:10,5:6", "4:20,5:10,6:3" };
 
     }
     public PlayerShop playerShop = new PlayerShop();
